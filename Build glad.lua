2f0ee136594706050441a6a19e8787c37cf05297
Lanz <ebersohnlanz@gmail.com>
2026-03-30 18:59:38 +0200

    Improved lua script

--- Build glad.lua
+++ Build glad.lua
@@ -1,47 +1,39 @@
 project "glad"
-	kind "StaticLib"
-	language "C"
-	staticruntime "off"
-	warnings "off"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+    warnings "off"
 
-	files {
-		"src/**.c",
-		"include/**.h",
-	}
+    files {
+        "src/**.c",
+        "include/**.h"
+    }
 
-    -- Add the include directories
-	includedirs {
-        "include",
-   }
+    includedirs { "include" }
 
-    -- Add the library directories
-   libdirs { }
-    
-    -- Add the dependant project
-    links { }
+    targetdir ("lib/" .. OutputDir .. "/%{prj.name}")
+    objdir    ("lib/intermediates/" .. OutputDir .. "/%{prj.name}")
 
-   targetdir ("lib/" .. OutputDir .. "/%{prj.name}")
-   objdir ("lib/intermediates/" .. OutputDir .. "/%{prj.name}")
+    filter "system:windows"
+        systemversion "latest"
 
-   filter "system:windows"
-       systemversion "latest"
-       defines { }
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "On"
 
-   filter "configurations:Debug"
-       defines { "DEBUG" }
-       runtime "Debug"
-       symbols "On"
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        runtime "Release"
+        optimize "On"
+        symbols "On"
 
-   filter "configurations:Release"
-       defines { "RELEASE" }
-       runtime "Release"
-       optimize "On"
-       symbols "On"
+    filter "configurations:Dist"
+        defines { "DIST" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
 
-   filter "configurations:Dist"
-       defines { "DIST" }
-       runtime "Release"
-       optimize "On"
-       symbols "Off"
     filter "action:vs*"
-        buildoptions { "/MP" }  -- Enable multi-process compilation
+        buildoptions { "/MP" }
+    filter {}
